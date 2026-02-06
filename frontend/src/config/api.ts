c5ecc5c116a6afb5afeb65a8df3789c7816e04a3
Shoban Chiddarth <messageshoban@gmail.com>
2026-02-06 11:32:36 +0530

    Updated backend url in frontend

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://finance-tracker-backend-af47.onrender.com',
+  baseURL: import.meta.env.VITE_API_URL || 'http://13.233.165.24:5000',
   headers: {
     'Content-Type': 'application/json',
   },
